788f798608ed89b8359fc71af22809b24b3469ae
ardra <ardra20172017@gmail.com>
2015-08-17 22:18:51 +0530

    add chapter5

---
+++ chapter5/ex1.c
@@ -0,0 +1,31 @@
+#include<ctype.c>
+
+int getch(void);
+void ungetch(int);
+
+int getint(int *pn)
+{
+  int c,d,sign;
+  while(isspace(c=getch()))
+    ;
+  if(!isdigit(c) && c!=EOF && c!='+' &&c!= '-'){
+    ungetch(c);
+    return 0;
+  }
+  sign==(c=='-')?-1:1;
+  if(c=='+'||c=='-'){
+    d=c;
+    if(!isdigit(c=getch())){
+	if(c!=EOF)
+	  ungetch(c);
+	return d;
+    }
+  }
+      
+  for(*pn=0;isdigit(c);c=getch())
+    *pn=10 * *pn+(c-'0');
+  *pn *=sign;
+  if(c!=EOF)
+    ungetch(c);
+  return c;
+}
